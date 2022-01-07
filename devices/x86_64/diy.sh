9edee25e0a7c86c899319d8fcc32ca597cf00df3
杨黄林 <yhl452493373@gmail.com>
2022-01-07 22:13:20 +0800

    Update diy.sh

--- devices/x86_64/diy.sh
+++ devices/x86_64/diy.sh
@@ -5,6 +5,9 @@ rm -rf target/linux/x86/patches-5.4/.svn
 
 sed -i 's,kmod-r8169,kmod-r8168,g' target/linux/x86/image/64.mk
 
+sed -i 's,"eth0 eth2 eth3" "eth1","eth1 eth2 eth3" "eth0",g' target/linux/x86/base-files/etc/board.d/02_network
+sed -i 's,"eth0 eth2 eth3 eth4 eth5 eth6 eth7" "eth1","eth1 eth2 eth3 eth4 eth5 eth6 eth7" "eth0",g' target/linux/x86/base-files/etc/board.d/02_network
+
 echo '
 CONFIG_CRYPTO_CHACHA20_X86_64=y
 CONFIG_CRYPTO_POLY1305_X86_64=y
